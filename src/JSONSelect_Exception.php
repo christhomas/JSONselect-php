c50ccee8fde3f1cc7259e02e58ee475d73c89b50
chris <chris@tourradar.com>
2015-08-17 19:13:43 +0200

    make a specialised exception that I can use to catch JSONSelect specific problems
    Signed-off-by: chris <chris@tourradar.com>

---
+++ src/JSONSelect_Exception.php
@@ -0,0 +1,8 @@
+<?php 
+class JSONSelect_Exception extends Exception
+{
+	public function __construct($message,$code = null, $previous = null)
+	{
+		parent::__construct($message,$code,$previous);
+	}
+}
